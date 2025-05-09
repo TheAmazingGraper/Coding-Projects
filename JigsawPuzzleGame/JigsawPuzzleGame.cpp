faced4b03deeff2636a358f744eaf02c3dc1a4b2
TheAmazingGraper <adammai18@gmail.com>
2025-05-08 19:40:20 -0500

    Added new code, a png, and the sln.

--- JigsawPuzzleGame/JigsawPuzzleGame.cpp
+++ JigsawPuzzleGame/JigsawPuzzleGame.cpp
@@ -1,11 +1,18 @@
 // Jigsaw Puzzle.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-
+using namespace std;
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+int main(){
+
+
+    //-------------------------------------------------
+    // New Window with a green circle
+    //-------------------------------------------------
+
+    
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -22,6 +29,92 @@ int main()
         window.draw(shape);
         window.display();
     }
+
+
+    //-------------------------------------------------
+    // Manipulating Angles
+    //-------------------------------------------------
+    sf::Angle angle1 = sf::degrees(10);
+    angle1 *= 2.f; // 20 degrees
+    sf::Angle angle2 = angle1 + sf::radians(0.5f); // 48.6 degrees
+    angle2 = -angle2; // -48.6 degrees
+    angle2 /= 3.f; // -16.2 degrees
+
+    bool equal = (angle1 == angle2); // false
+    bool inequal = (angle1 != angle2); // true
+
+    cout << "Manipulating Angles" << endl;
+
+    cout << "Angle1: " << angle1.asDegrees() << endl;
+    cout << "Angle1: " << angle1.asRadians() << endl;
+
+    cout << "Angle2: " << angle2.asDegrees() << endl;
+    cout << "Angle2: " << angle2.asRadians() << endl;
+
+    cout << "Is Angle1 = Angle2 : " << equal << endl;
+    cout << "Is Angle1 = Angle2 : " << inequal << endl;
+
+    //-------------------------------------------------
+    // Clock and Time
+    //-------------------------------------------------
+    
+    cout << "\nClock and Time" << endl;
+
+    sf::Time t1 = sf::microseconds(10000);
+    sf::Time t2 = sf::milliseconds(10);
+    sf::Time t3 = sf::seconds(0.01f);
+
+    cout << "t1: " << t1.asMicroseconds() << " microseconds" << endl;
+    cout << "t2: " << t2.asMilliseconds() << " milliseconds" << endl;
+    cout << "t3: " << t3.asSeconds() << " seconds" << endl;
+
+    sf::Clock clock; // starts the clock
+    
+    //Time is constant, so we need to update it.
+    sf::Time elapsed1 = clock.getElapsedTime(); 
+    cout << "elapsed1: " << elapsed1.asMicroseconds() << " microseconds" << endl;
+    clock.restart();
+
+    elapsed1 = clock.getElapsedTime();
+    sf::Time elapsed2 = clock.getElapsedTime();
+    cout << "elapsed2: " << elapsed2.asMilliseconds() << " milliseconds" << endl;
+    clock.restart();
+    
+    elapsed1 = clock.getElapsedTime();
+    sf::Time elapsed3 = clock.getElapsedTime();
+    std::cout << "elapsed3: " << elapsed3.asSeconds() << " seconds" << endl;
+
+    clock.stop(); // stops the clock
+    cout << "Is the clock running: " << boolalpha << clock.isRunning() << endl; // boolalpha transform the bool into "true / false" instead of "1 / 0"
+    clock.reset(); // resets elapsed time to zero, and also stop running the clock.
+
+    clock.start(); // starts the clock
+    sf::Time elapsed4 = clock.getElapsedTime();
+    cout << "elapsed4: " << elapsed4.asSeconds() << " seconds" << endl;
+
+    /*
+    sf::Clock clock;
+    while (window.isOpen())
+    {
+        sf::Time elapsed = clock.restart();
+        updateGame(elapsed);
+        ...
+    }*/
+
+    //-------------------------------------------------
+    // User Streams (input and output)
+    //-------------------------------------------------
+
+
+    //Using an input stream
+
+    sf::FileInputStream stream("Grapes.png");
+    sf::Texture texture(stream);
+
+    //-------------------------------------------------
+    // Attempting to open another window with the Grapes.png
+    //-------------------------------------------------
+
 }
 
 
